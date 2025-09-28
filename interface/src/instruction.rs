5a570d67fa12aca8efe9a6e15d90b22e8b2d6338
Matt <90358481+xbtmatt@users.noreply.github.com>
2025-09-28 00:04:22 -0700

    Add comments indicating future work/todo

--- interface/src/instruction.rs
+++ interface/src/instruction.rs
@@ -18,12 +18,46 @@ impl TryFrom<u8> for InstructionTag {
     fn try_from(value: u8) -> Result<Self, Self::Error> {
         match value {
             // SAFETY: A valid enum variant is guaranteed with the match pattern.
+            // All variants are checked in the exhaustive instruction tag test.
             0..4 => Ok(unsafe { core::mem::transmute::<u8, Self>(value) }),
             _ => Err(DropsetError::InvalidInstructionTag.into()),
         }
     }
 }
 
+////////////////////
+/// TODO: Add the load::<T>(data: &[u8]) -> &T      unsafe { *.as_ptr() as *const T } style
+///       slice dereferencing with tags + enums.
+///
+/// that is, all fields would be 1-aligned slices, with getters/setters on each field's raw
+/// pointer slice
+///
+/// const asserts on align-1 and size_of::<T>()
+///
+/// This couples the enum tag with the enum data in an intuitive manner
+/// without having to copy *and* automatically avoids zero-init. Makes things much cleaner and
+/// easier to iterate on.
+///
+/// with load::<T> passing ixn data around is ultimately just passing slice references around
+///
+/// it also means fewer checks on packing and unpacking because you can just do a single check
+/// on the slice LEN as opposed to a check for each field
+///
+/// it would even be nice to have a proc macro for this:
+///
+/// ```
+/// #[derive(Align1IxData)]
+/// #[ix(tag = 3)] // Transfer
+/// #[repr(C)]
+/// pub struct TransferData {
+///     // fields represented as slices of types with endianness must be private
+///     // representing these as slices avoids alignment issues (dereferencing an unaligned u64 is
+///     // UB)
+///     amount: [u8; 8],
+///     // only endian-agnostic values should be `pub` (and can thus forgo getters/setters)
+///     pub decimals:  u8,
+/// }
+/// ```
 #[cfg(test)]
 mod tests {
     use super::InstructionTag;
