e6496e6cf61a77bf9ca134c5d8f12a8a22adb468
Matt <90358481+xbtmatt@users.noreply.github.com>
2025-10-01 17:43:45 -0700

    Reword some comments, clean things up, use `zero_out_payload` function, set `prev` to zero on add too, and use the non nil sector index in free_stack.rs

--- interface/src/state/free_stack.rs
+++ interface/src/state/free_stack.rs
@@ -2,13 +2,13 @@ use crate::{
     error::{DropsetError, DropsetResult},
     state::{
         node::{Node, NodePayload, NODE_PAYLOAD_SIZE},
-        sector::{LeSectorIndex, SectorIndex},
+        sector::{LeSectorIndex, NonNilSectorIndex, SectorIndex},
         transmutable::Transmutable,
     },
 };
 
 pub struct Stack<'a> {
-    /// The reference to the LE bytes tracking the node at the top of the stack's sector index.
+    /// The reference to the sector index as LE bytes for the node at the top of the stack.
     top: &'a mut LeSectorIndex,
     sectors: &'a mut [u8],
 }
@@ -23,26 +23,28 @@ unsafe impl Transmutable for FreeNodePayload {
 impl NodePayload for FreeNodePayload {}
 
 impl<'a> Stack<'a> {
-    pub fn new(sectors: &'a mut [u8], top: &'a mut LeSectorIndex) -> Self {
+    pub fn new_from_parts(top: &'a mut LeSectorIndex, sectors: &'a mut [u8]) -> Self {
         Stack { top, sectors }
     }
 
     pub fn push_free_node(&mut self, index: SectorIndex) -> DropsetResult {
         let node = Node::from_sector_index_mut(self.sectors, index)?;
-        // Zero out the node's payload bytes.
-        let payload = node.load_payload_mut::<FreeNodePayload>();
-        payload.0 = [0; NODE_PAYLOAD_SIZE];
+        node.zero_out_payload();
 
-        // Then set the `next` node to `top`, and `top` to `index`.
+        // This isn't strictly necessary since `prev` should never be used in a free stack node
+        // but it's a cheap call and makes the data cleaner/simpler.
+        node.set_prev(SectorIndex(0));
+
+        // Set the `next` node to `top`, and `top` to `index`.
         node.set_next(self.top.get());
         self.set_top(index);
 
         Ok(())
     }
 
-    pub fn remove_free_node(&mut self) -> Result<SectorIndex, DropsetError> {
+    pub fn remove_free_node(&mut self) -> Result<NonNilSectorIndex, DropsetError> {
         if self.top().is_nil() {
-            return Ok(self.top());
+            return Err(DropsetError::NoFreeNodesLeft);
         }
 
         // The free node is the node at the top of the stack.
@@ -52,12 +54,12 @@ impl<'a> Stack<'a> {
         // Set the new `top` to the current top's `next`.
         self.top.set(node_being_freed.next());
 
-        // Zero out the rest of the node by setting `next` to 0. The payload was zeroed when adding
-        // to the free list, and `prev` is unused bytes.
+        // Zero out the rest of the node by setting `next` to 0. The payload and `prev` were zeroed
+        // out when adding to the free list.
         node_being_freed.set_next(SectorIndex(0));
 
         // And return the index of the freed node.
-        Ok(free_index)
+        Ok(NonNilSectorIndex::new_unchecked(free_index))
     }
 
     pub fn top(&self) -> SectorIndex {
