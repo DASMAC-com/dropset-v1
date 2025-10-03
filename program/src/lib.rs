f13b7b59deec9ca377315d43ff9f45bdce06fb10
Matt <90358481+xbtmatt@users.noreply.github.com>
2025-10-02 17:43:53 -0700

    Add `declare_id` to program/lib.rs

--- program/src/lib.rs
+++ program/src/lib.rs
@@ -8,3 +8,5 @@ mod instructions;
 program_entrypoint!(entrypoint::process_instruction);
 no_allocator!();
 nostd_panic_handler!();
+
+pinocchio_pubkey::declare_id!("TESTnXwv2eHoftsSd5NEdpH4zEu7XRC8jviuoNPdB2Q");
