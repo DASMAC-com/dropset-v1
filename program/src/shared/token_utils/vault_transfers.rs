75f24e803387d01e70aaaf2a1345c0c0e36d1e1a
Matt <90358481+xbtmatt@users.noreply.github.com>
2025-10-04 14:18:01 -0700

    Add withdraw_from_vault with PDA signer

--- program/src/shared/token_utils/vault_transfers.rs
+++ program/src/shared/token_utils/vault_transfers.rs
@@ -1,6 +1,7 @@
+use dropset_interface::state::market::MarketRef;
 use pinocchio::{program_error::ProgramError, ProgramResult};
 
-use crate::context::deposit_withdraw_context::DepositWithdrawContext;
+use crate::{context::deposit_withdraw_context::DepositWithdrawContext, market_signer};
 
 pub fn deposit_to_vault(ctx: &DepositWithdrawContext, amount: u64) -> Result<u64, ProgramError> {
     if ctx.token_program.is_spl_token {
@@ -11,11 +12,11 @@ pub fn deposit_to_vault(ctx: &DepositWithdrawContext, amount: u64) -> Result<u64
             amount,
         }
         .invoke()?;
+
         // `spl_token` always transfers the exact amount passed in.
         Ok(amount)
     } else {
         let decimals = ctx.mint.get_mint_decimals()?;
-
         let balance_before = ctx.vault_ata.get_balance()?;
 
         pinocchio_token_2022::instructions::TransferChecked {
@@ -28,6 +29,7 @@ pub fn deposit_to_vault(ctx: &DepositWithdrawContext, amount: u64) -> Result<u64
             token_program: ctx.token_program.info.key(),
         }
         .invoke()?;
+
         let balance_after = ctx.vault_ata.get_balance()?;
         // `spl_token_2022` amount deposited must be checked due to transfer hooks, fees, and other
         // extensions that may intercept a simple transfer and alter the amount transferred.
@@ -38,6 +40,33 @@ pub fn deposit_to_vault(ctx: &DepositWithdrawContext, amount: u64) -> Result<u64
     }
 }
 
-pub fn withdraw_from_vault(_ctx: &DepositWithdrawContext, _amount: u64) -> ProgramResult {
-    todo!()
+pub fn withdraw_from_vault(ctx: &DepositWithdrawContext, amount: u64) -> ProgramResult {
+    let (base_mint, quote_mint, market_bump) = {
+        // Safety: Single immutable borrow to the market account data.
+        let data = unsafe { ctx.market_account.info.borrow_data_unchecked() };
+        let market = MarketRef::from_bytes_unchecked(data)?;
+        let header = market.header;
+        (header.base_mint, header.quote_mint, header.market_bump)
+    };
+
+    if ctx.token_program.is_spl_token {
+        pinocchio_token::instructions::Transfer {
+            from: ctx.vault_ata.info,
+            to: ctx.user_ata.info,
+            authority: ctx.market_account.info,
+            amount,
+        }
+        .invoke_signed(&[market_signer!(base_mint, quote_mint, market_bump)])
+    } else {
+        let decimals = ctx.mint.get_mint_decimals()?;
+        pinocchio_token::instructions::TransferChecked {
+            from: ctx.vault_ata.info,
+            to: ctx.user_ata.info,
+            authority: ctx.market_account.info,
+            amount,
+            mint: ctx.mint.info,
+            decimals,
+        }
+        .invoke_signed(&[market_signer!(base_mint, quote_mint, market_bump)])
+    }
 }
