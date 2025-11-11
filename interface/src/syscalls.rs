87a228f35d6fa95bcba37faa117030a5d9c917cc
Matt <90358481+xbtmatt@users.noreply.github.com>
2025-11-11 14:11:59 -0800

    Remove unused syscalls and expose all as `unsafe`

--- interface/src/syscalls.rs
+++ interface/src/syscalls.rs
@@ -4,6 +4,7 @@
 //! Copied from `pinocchio` – commit bde84880a6709bbda4da8767b5f0a42d9678d07c
 //!
 //! Modifications made:
+//! - Removed unused syscalls
 //! - Changed some formatting
 //! - Changed function argument names to match the rust [`core::ptr`] calls.
 //! - Added fallbacks for non-solana targets; i.e., when `#[cfg(not(target_os = "solana"))]`.
@@ -11,61 +12,43 @@
 //! Original: <https://github.com/anza-xyz/pinocchio/blob/bde84880a6709bbda4da8767b5f0a42d9678d07c/sdk/log/crate/src/logger.rs>
 
 #[cfg(all(target_os = "solana", not(target_feature = "static-syscalls")))]
+/// Syscalls provided by the SVM runtime (SBPFv0, SBPFv1 and SBPFv2).
 mod inner {
-    // Syscalls provided by the SVM runtime (SBPFv0, SBPFv1 and SBPFv2).
     extern "C" {
-        pub fn sol_log_(message: *const u8, len: u64);
-
         pub fn sol_memcpy_(dst: *mut u8, src: *const u8, count: u64);
 
         pub fn sol_memset_(dst: *mut u8, val: u8, count: u64);
-
-        pub fn sol_remaining_compute_units() -> u64;
     }
 }
 
 #[cfg(all(target_os = "solana", target_feature = "static-syscalls"))]
+/// Syscalls provided by the SVM runtime (SBPFv3 and newer).
 mod inner {
-    // Syscalls provided by the SVM runtime (SBPFv3 and newer).
-    pub(crate) fn sol_log_(message: *const u8, length: u64) {
-        // murmur32 hash of "sol_log_"
-        let syscall: extern "C" fn(*const u8, u64) = unsafe { core::mem::transmute(544561597u64) };
-        syscall(message, length)
-    }
-
-    pub(crate) fn sol_memcpy_(dst: *mut u8, src: *const u8, count: u64) {
+    pub(crate) unsafe fn sol_memcpy_(dst: *mut u8, src: *const u8, count: u64) {
         // murmur32 hash of "sol_memcpy_"
         let syscall: extern "C" fn(*mut u8, *const u8, u64) =
             unsafe { core::mem::transmute(1904002211u64) };
         syscall(dst, src, count)
     }
 
-    pub(crate) fn sol_memset_(dst: *mut u8, val: u8, count: u64) {
+    pub(crate) unsafe fn sol_memset_(dst: *mut u8, val: u8, count: u64) {
         // murmur32 hash of "sol_memset_"
         let syscall: extern "C" fn(*mut u8, u8, u64) =
             unsafe { core::mem::transmute(930151202u64) };
         syscall(dst, val, count)
     }
-
-    pub(crate) fn sol_remaining_compute_units() -> u64 {
-        // murmur32 hash of "sol_remaining_compute_units"
-        let syscall: extern "C" fn() -> u64 = unsafe { core::mem::transmute(3991886574u64) };
-        syscall()
-    }
 }
 
 #[cfg(not(target_os = "solana"))]
 #[allow(dead_code)]
+/// Syscall fallbacks for non-`solana` targets.
 mod inner {
-    pub(crate) fn sol_log_(_message: *const u8, _length: u64) {}
-
     /// Copies `count` bytes from `src` to `dst`.
     ///
-    /// This function is not marked as `unsafe` so its function signature matches the other syscall
-    /// implementations.
+    /// # Safety
     ///
-    /// However, caller should adhere to the safety contract in [`core::ptr::copy_nonoverlapping`].
-    pub(crate) fn sol_memcpy_(dst: *mut u8, src: *const u8, count: u64) {
+    /// Caller should adhere to the safety contract in [`core::ptr::copy_nonoverlapping`].
+    pub(crate) unsafe fn sol_memcpy_(dst: *mut u8, src: *const u8, count: u64) {
         unsafe {
             core::ptr::copy_nonoverlapping(src, dst, count as usize);
         }
@@ -73,20 +56,15 @@ mod inner {
 
     /// Sets `count` bytes of memory to `val`, starting at `dst`.
     ///
-    /// This function is not marked as `unsafe` so its function signature matches the other syscall
-    /// implementations.
+    /// # Safety
     ///
     /// However, caller should adhere to the safety contract in [`core::ptr::write_bytes`].
-    pub(crate) fn sol_memset_(dst: *mut u8, val: u8, count: u64) {
+    pub(crate) unsafe fn sol_memset_(dst: *mut u8, val: u8, count: u64) {
         unsafe {
             core::ptr::write_bytes(dst, val, count as usize);
         }
     }
-
-    pub(crate) fn sol_remaining_compute_units() -> u64 {
-        0
-    }
 }
 
 #[allow(unused_imports)]
-use inner::*;
+pub(crate) use inner::*;
