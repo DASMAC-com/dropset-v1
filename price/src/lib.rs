56fc0f9c3da9119323ff8a1dae59a74440c7ff60
Matt <90358481+xbtmatt@users.noreply.github.com>
2025-11-25 18:15:34 -0800

    Fix checks, remove type assertions, check significand final result range

--- price/src/lib.rs
+++ price/src/lib.rs
@@ -1,11 +1,8 @@
-use static_assertions::const_assert;
+/// The number of significant digits in the significand; i.e., the digits represented in the price.
+pub const SIGNIFICANT_DIGITS: u8 = 9;
 
-/// The number of significant digits in the significant; i.e., the digits represented in the price.
-///
-/// For example, for a price of 12.413 and [`SIGNIFICANT_DIGITS`] == 8:
-///
-/// THe significand/price would be `12_413_000`.
-pub const SIGNIFICANT_DIGITS: u8 = 8;
+const MAX_SIGNIFICAND: u64 = 999_999_999;
+const MIN_SIGNIFICAND: u64 = 100_000_000;
 
 #[repr(C)]
 pub struct Price {
@@ -20,48 +17,49 @@ pub enum PriceError {
     InvalidLotExponent,
     InvalidTickExponent,
     LotMinusTickUnderflow,
+    ArithmeticOverflow,
+    InvalidSignificand,
 }
-
-type SignificandType = u32;
-type LotsType = u16;
-
-#[allow(clippy::absurd_extreme_comparisons)]
-const _: () = {
-    const_assert!((LotsType::MAX as u64 * SignificandType::MAX as u64) <= u64::MAX);
-};
 
 pub fn to_price(
     significand: u32,
-    lots: u16,
+    lots: u64,
     lot_exp: u8,
     tick_exp: u8,
 ) -> Result<Price, PriceError> {
-    // This is only for compile-time type checking. It ensures that the const assertion types
-    // reflect the types passed to this function.
-    let _: &SignificandType = &significand;
-    let _: &LotsType = &lots;
-
-    let lots = lots as u64;
     let significand = significand as u64;
 
     let base = lots
         .checked_mul(pow10_u64!(lot_exp, PriceError::InvalidLotExponent))
         .ok_or(PriceError::InvalidLotExponent)?;
 
-    // Note: significant * lots is always <= u64::MAX, checked with const asserts above.
-    let quote = (significand * lots)
+    let significand_times_lots = significand
+        .checked_mul(lots)
+        .ok_or(PriceError::ArithmeticOverflow)?;
+
+    let quote = significand_times_lots
         .checked_mul(pow10_u64!(tick_exp, PriceError::InvalidLotExponent))
-        .ok_or(PriceError::InvalidLotExponent)?;
+        .ok_or(PriceError::ArithmeticOverflow)?;
 
     if lot_exp > tick_exp {
         return Err(PriceError::LotMinusTickUnderflow);
     }
-    let price_exp = pow10_u64!(tick_exp - lot_exp, PriceError::InvalidLotExponent);
-    let price = significand
-        .checked_mul(price_exp)
-        .ok_or(PriceError::InvalidLotExponent)?;
+    // Safety: the underflow condition was just checked; it returns early if underflow would occur.
+    let price_exp = unsafe { tick_exp.unchecked_sub(lot_exp) };
 
-    Ok(Price { price, base, quote })
+    // Safety:
+    // The mult operation here is always strictly <= (significand * lots) * tick_exp, since
+    // price_exp == tick_exp - lot_exp.
+    // This means that the multiplication operation here does not need to be checked, as it is
+    // guaranteed to not overflow since the `quote` calculation did not overflow.
+    let price =
+        unsafe { significand.unchecked_mul(pow10_u64!(price_exp, PriceError::InvalidLotExponent)) };
+
+    if (MIN_SIGNIFICAND..MAX_SIGNIFICAND).contains(&price) {
+        Ok(Price { price, base, quote })
+    } else {
+        Err(PriceError::InvalidSignificand)
+    }
 }
 
 /// Returns `10^exp` inline using a `match` on the exponent.
@@ -106,7 +104,10 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let _result = to_price(2, 2, 3, 4).expect("Should calculate price");
+    fn happy_path_simple_price() {
+        let price = to_price(1234, 1, 0, 0).expect("Should calculate price");
+        assert_eq!(price.base, 1);
+        assert_eq!(price.quote, 1234);
+        assert_eq!(price.price, 1234);
     }
 }
