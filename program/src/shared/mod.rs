352eacdcfa60a79a4e7efc65cff7b2c3841026da
Matt <90358481+xbtmatt@users.noreply.github.com>
2025-12-16 14:27:46 -0800

    Move around shared operations stuff so the market operations are separate from seat operations

--- program/src/shared/mod.rs
+++ program/src/shared/mod.rs
@@ -1,6 +1,7 @@
 //! Shared utilities and helpers for `dropset` program logic.
 
 pub mod account_resize;
+pub mod market_operations;
 pub mod order_operations;
 pub mod seat_operations;
 pub mod seeds;
--- program/src/shared/seat_operations.rs
+++ program/src/shared/seat_operations.rs
@@ -4,20 +4,16 @@ use dropset_interface::{
     error::DropsetError,
     state::{
         market::{
-            Market,
             MarketRef,
             MarketRefMut,
         },
-        market_header::MarketHeader,
         market_seat::MarketSeat,
         node::Node,
         seats_dll::SeatsLinkedList,
         sector::{
             SectorIndex,
             NIL,
-            SECTOR_SIZE,
         },
-        transmutable::Transmutable,
     },
 };
 use pinocchio::pubkey::{
@@ -115,135 +111,5 @@ pub unsafe fn find_mut_seat_with_hint<'a>(
         Ok(seat)
     } else {
         Err(DropsetError::InvalidIndexHint)
-    }
-}
-
-/// Initializes a freshly created market account. This function skips checks based on the assumption
-/// that the market has just been created on-chain.
-///
-/// This function should *only* be called atomically in the same instruction that the market account
-/// is created or in tests.
-pub fn initialize_market_account_data<'a>(
-    zeroed_market_account_data: &'a mut [u8],
-    base_mint: &Pubkey,
-    quote_mint: &Pubkey,
-    market_bump: u8,
-) -> Result<MarketRefMut<'a>, DropsetError> {
-    let account_data_len = zeroed_market_account_data.len();
-    if account_data_len < MarketHeader::LEN {
-        return Err(DropsetError::UnallocatedAccountData);
-    }
-
-    let sector_bytes = account_data_len - MarketHeader::LEN;
-
-    if sector_bytes % SECTOR_SIZE != 0 {
-        return Err(DropsetError::UnalignedData);
-    }
-
-    // Safety: The account's data length was verified as at least `MARKET_HEADER_SIZE`.
-    let mut market = unsafe { Market::from_bytes_mut(zeroed_market_account_data) };
-
-    // Safety: The zeroed market account data is exclusively mutably borrowed and length-verified.
-    unsafe {
-        // Initialize the market header.
-        MarketHeader::init(
-            core::ptr::addr_of_mut!(*market.header),
-            market_bump,
-            base_mint,
-            quote_mint,
-        );
-    }
-
-    // Initialize all sectors by adding them to the free stack.
-    let stack = &mut market.free_stack();
-    let num_sectors = sector_bytes / SECTOR_SIZE;
-
-    // Safety
-    // Both indices are in-bounds, `start` < `end`, and the caller guarantees that the
-    // account was just created, meaning it's entirely zeroed out bytes.
-    unsafe { stack.convert_zeroed_bytes_to_free_nodes(0, num_sectors as u32) }?;
-
-    Ok(market)
-}
-
-#[cfg(test)]
-pub mod tests {
-    use dropset_interface::state::{
-        market_seat::MarketSeat,
-        sector::SECTOR_SIZE,
-        transmutable::Transmutable,
-    };
-    use pinocchio_pubkey::pubkey;
-
-    use super::initialize_market_account_data;
-
-    extern crate std;
-    use std::{
-        vec,
-        vec::Vec,
-    };
-
-    use super::*;
-
-    #[test]
-    fn market_insert_users() {
-        const N_SECTORS: usize = 10;
-        let mut bytes = [0u8; MarketHeader::LEN + SECTOR_SIZE * N_SECTORS];
-        let mut market = initialize_market_account_data(
-            bytes.as_mut(),
-            &pubkey!("11111111111111111111111111111111111111111111"),
-            &pubkey!("22222222222222222222222222222222222222222222"),
-            254,
-        )
-        .expect("Should initialize market data");
-
-        let mut seat_list = market.seat_list();
-
-        let [zero, one, two, three, ten, forty] = vec![
-            [vec![0; 31], vec![0]].concat().try_into().unwrap(),
-            [vec![0; 31], vec![1]].concat().try_into().unwrap(),
-            [vec![0; 31], vec![2]].concat().try_into().unwrap(),
-            [vec![0; 31], vec![3]].concat().try_into().unwrap(),
-            [vec![0; 31], vec![10]].concat().try_into().unwrap(),
-            [vec![0; 31], vec![40]].concat().try_into().unwrap(),
-        ]
-        .into_iter()
-        .enumerate()
-        .map(|(i, pk)| MarketSeat::new(pk, i as u64, (i + 2) as u64))
-        .collect::<Vec<MarketSeat>>()
-        .try_into()
-        .unwrap();
-
-        let seats: Vec<MarketSeat> = vec![
-            three.clone(),
-            two.clone(),
-            forty.clone(),
-            zero.clone(),
-            ten.clone(),
-            one.clone(),
-        ];
-
-        seats.clone().into_iter().for_each(|seat| {
-            assert!(try_insert_market_seat(&mut seat_list, seat).is_ok());
-        });
-
-        let resulting_seat_list: Vec<(SectorIndex, &MarketSeat)> = seat_list
-            .iter()
-            .map(|(i, node)| (i, node.load_payload::<MarketSeat>()))
-            .collect();
-
-        let expected_order = vec![zero, one, two, three, ten, forty];
-
-        // Check lengths before zipping.
-        assert_eq!(expected_order.len(), resulting_seat_list.len());
-
-        for (expected, actual) in resulting_seat_list
-            .into_iter()
-            .zip(expected_order.into_iter().enumerate())
-        {
-            // The `actual` user pubkeys should match the `expected` order.
-            let (pk_e, pk_a) = (expected.1, &actual.1);
-            assert_eq!(pk_e, pk_a);
-        }
     }
 }
