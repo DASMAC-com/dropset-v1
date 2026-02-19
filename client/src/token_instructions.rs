482f65116a8eb0c9568e91fd6df4a915ab3d620e
Matt <90358481+xbtmatt@users.noreply.github.com>
2026-02-19 14:52:08 -0700

    Make variable name more explicit about the mint authority being the payer

--- client/src/token_instructions.rs
+++ client/src/token_instructions.rs
@@ -4,14 +4,14 @@ use solana_sdk::program_pack::Pack;
 use spl_token_interface::state::Mint;
 
 pub fn create_and_initialize_token_instructions(
-    mint_authority: &Address,
+    mint_authority_and_payer: &Address,
     mint: &Address,
     rent_lamports: u64,
     mint_decimals: u8,
     token_program: &Address,
 ) -> anyhow::Result<(Instruction, Instruction)> {
     let create_mint_account = solana_system_interface::instruction::create_account(
-        mint_authority,
+        mint_authority_and_payer,
         mint,
         rent_lamports,
         Mint::LEN as u64,
@@ -21,7 +21,7 @@ pub fn create_and_initialize_token_instructions(
     let initialize_mint = spl_token_2022_interface::instruction::initialize_mint2(
         token_program,
         mint,
-        mint_authority,
+        mint_authority_and_payer,
         None,
         mint_decimals,
     )?;
